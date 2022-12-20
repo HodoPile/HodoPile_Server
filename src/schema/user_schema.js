3238c1a8f0fcaf65aff42f53dbfbc4779207e06c
antoni <antoni.dev0@gmail.com>
2022-12-20 09:34:50 -0800

    add user and favorites routes, add route handlers. Add handler for user get all, get one, post one, and  update one. Add handler for favorites get all and update one

--- src/schema/user_schema.js
+++ src/schema/user_schema.js
@@ -1,17 +1,18 @@
 "use strict";
 
 const { mongoose, Schema } = require("mongoose");
+
 // TODO: add updated_at field
 const  userSchema = new Schema({
     name: { type: String, required: true },
     nickname: { type: String, default: "lorem ipsum" },
     picture: { type: String, default: "lorem ipsum" },
-    sub: { type: String, default:""},
+    sub_id: { type: String, default:""},
     email: { type: String, default:""},
     email_verified: { type: String, default:""},
     favorited: [{ type: Schema.Types.ObjectId }]
 },{ collection: "users" })
 
-const User = mongoose.model("Destination", userSchema)
+const User = mongoose.model("User", userSchema)
 
 module.exports = { User }
